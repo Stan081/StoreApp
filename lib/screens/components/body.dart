89bf9e3bd3a6184903d73599596fb5d7d28b3a7e
Stanley <egbestanley84@yahoo.com>
2022-04-28 06:29:05 +0100

    mplemented the category componenton the home screen

--- lib/screens/components/body.dart
+++ lib/screens/components/body.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:store_app/constants.dart';
+import 'package:store_app/models/Product.dart';
+import 'package:store_app/screens/components/categories.dart';
 
 class Body extends StatelessWidget {
   const Body({Key? key}) : super(key: key);
@@ -7,6 +9,7 @@ class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: kDefaultPaddin),
@@ -17,6 +20,28 @@ class Body extends StatelessWidget {
                 .headline5
                 ?.copyWith(fontWeight: FontWeight.bold),
           ),
+        ),
+        Categories(),
+        Container(
+          padding: const EdgeInsets.all(kDefaultPaddin),
+          height: 180,
+          width: 160,
+          decoration: BoxDecoration(
+            color: products[0].color,
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: Image.asset(products[0].image),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: kDefaultPaddin / 4),
+          child: Text(
+            products[0].title,
+            style: const TextStyle(color: kTextLightColor),
+          ),
+        ),
+        const Text(
+          "\$234",
+          style: TextStyle(fontWeight: FontWeight.bold),
         )
       ],
     );
